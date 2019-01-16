aaef832bb9b38ce3c778bb721da0f56fbcc147a2
Andre Luis Miranda <almiranda86@gmail.com>
2019-01-16 18:13:13 -0200

    Add - Correção paleativa para conexão com o banco de dados, para a chamada do .Net Core

--- Sagui.Base/Factory/DbFactory.cs
+++ Sagui.Base/Factory/DbFactory.cs
@@ -14,48 +14,41 @@ namespace Sagui.Base.Factory
 {
     public sealed class DbFactory
     {
-        public static DALBaseHandler sectionHandler = (DALBaseHandler)ConfigurationManager.GetSection("DALBaseHandler");
-        private DbFactory() { }
-        public static DALBase CreateDataBase()
-        {
-            if (sectionHandler?.Name?.Length == 0) throw new Exception("Database name not defined in DbFactoryConfiguration section of config file");
-            try
-            {
-                Type database = Type.GetType(sectionHandler.Name);
-                ConstructorInfo constructorInfo = database.GetConstructor(new Type[] { });
-                DALBase databaseObj = (DALBase)constructorInfo.Invoke(null);
-                databaseObj.connectionString = sectionHandler.ConnectionString;
-                return databaseObj;
-            }
-            catch (Exception excep)
-            {
-                throw new Exception("Error instantiating database " + sectionHandler.Name + ". " + excep.Message);
-            }
-        }
-
-        //static string HandlerName = "";
-
-        //static string HandlerConnection = default(string);
-
-        //public DbFactory() { }
-
+        //public static DALBaseHandler sectionHandler = (DALBaseHandler)ConfigurationManager.GetSection("DALBaseHandler");
+        //private DbFactory() { }
         //public static DALBase CreateDataBase()
         //{
-        //    HandlerName = "Sagui.Base.DAL.DALBaseSQL";
-        //    HandlerConnection = ConfigurationManager.ConnectionStrings[0].ConnectionString;
-
+        //    if (sectionHandler?.Name?.Length == 0) throw new Exception("Database name not defined in DbFactoryConfiguration section of config file");
         //    try
         //    {
-        //        Type database = Type.GetType(HandlerName);
+        //        Type database = Type.GetType(sectionHandler.Name);
         //        ConstructorInfo constructorInfo = database.GetConstructor(new Type[] { });
         //        DALBase databaseObj = (DALBase)constructorInfo.Invoke(null);
-        //        databaseObj.connectionString = HandlerConnection;
+        //        databaseObj.connectionString = sectionHandler.ConnectionString;
         //        return databaseObj;
         //    }
         //    catch (Exception excep)
         //    {
-        //        throw new Exception("Error instantiating database " + HandlerName + ". " + excep.Message);
+        //        throw new Exception("Error instantiating database " + sectionHandler.Name + ". " + excep.Message);
         //    }
         //}
+
+        public DbFactory() { }
+
+        public static DALBase CreateDataBase()
+        {
+            try
+            {
+                Type database = Type.GetType(Comum.DALComum.HandlerName);
+                ConstructorInfo constructorInfo = database.GetConstructor(new Type[] { });
+                DALBase databaseObj = (DALBase)constructorInfo.Invoke(null);
+                databaseObj.connectionString = Comum.DALComum.HandlerConnection;
+                return databaseObj;
+            }
+            catch (Exception excep)
+            {
+                throw new Exception("Error instantiating database " + Comum.DALComum.HandlerConnection + ". " + excep.Message);
+            }
+        }
     }
 }
