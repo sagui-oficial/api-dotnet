101a06a11de8dee404522829a591babb671acb94
Fabio Santos <fabio.silva@tecfil.com.br>
2019-02-05 08:23:01 -0200

    REMOVIDOS PROJETOS FULLFRAMEWORK

--- Sagui.Application/Startup.cs
+++ Sagui.Application/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,9 @@ namespace Sagui.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<Sagui.DBNN.Sagui>(options =>
+            //     options.UseSqlServer(Configuration.GetConnectionString("ConnectionStringName")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
---
+++ Sagui.DBNN/Sagui.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sagui.Model;
+using System;
+
+namespace Sagui.DBNN
+{
+    
+    public class Sagui : DbContext
+    {
+        public Sagui(DbContextOptions<Sagui> options) : base(options)
+        {
+        }
+
+        public DbSet<Arquivo_GTO> Arquivo_GTO { get; set; }
+        public DbSet<Arquivos> Arquivo { get; set; }
+        public DbSet<GTO> GTO { get; set; }
+        public DbSet<PlanoOperadora> PlanoOperadora { get; set; }
+        public DbSet<Procedimento_GTO> Procedimento_GTO { get; set; }
+        public DbSet<Procedimentos> Procedimento { get; set; }
+        public DbSet<UsuarioBase> UsuarioBase { get; set; }
+        public DbSet<PlanoOperadoraPaciente> PlanoOperadoraPaciente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Course>().ToTable("Course");
+            //modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            //modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+    
+}
