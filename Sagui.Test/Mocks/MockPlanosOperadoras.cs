dbafe00f52f5a26234d4508766f06da046e51ccf
Jomal Diego <jomaldiego@gmail.com>
2019-03-13 21:15:51 -0300

    Criação de PlanoOperadora

--- Sagui.Test/Mocks/MockPlanosOperadoras.cs
+++ Sagui.Test/Mocks/MockPlanosOperadoras.cs
@@ -1,4 +1,5 @@
-﻿using Sagui.Service.RequestResponse;
+﻿using Sagui.Model;
+using Sagui.Service.RequestResponse;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,21 @@ namespace Sagui.Test.Mocks
 {
     public class MockPlanoOperadora
     {
+        
         public RequestPlanoOperadora CriarMockPlanoOperadora()
         {
+            MockProcedimento mockProcedimento = new MockProcedimento();
+            MockArquivos mockArquivos = new MockArquivos();
+
             RequestPlanoOperadora planoOperadora = new RequestPlanoOperadora();
+            planoOperadora.Id = 1;
+            planoOperadora.NomeFantasia = "AMIL";
+            planoOperadora.RazaoSocial = "AMIL";
+            planoOperadora.CNPJ = "0000000000191";
+            planoOperadora.DataEnvioLote = new DateTime();
+            planoOperadora.DataRecebimentoLote = new DateTime();
+            planoOperadora.ListaProcedimentos = new List<Model.Procedimentos>() { mockProcedimento.CriarMockProcedimento() };
+            planoOperadora.ListaArquivos = new List<Arquivos>() { mockArquivos.CriarMockArquivo() };
 
             return planoOperadora;
         }
