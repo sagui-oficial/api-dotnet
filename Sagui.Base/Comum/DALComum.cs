db8c9b701dcd690937d32f0366318a79109cb137
Fabio Santos <fabio.silva@tecfil.com.br>
2019-05-08 16:15:54 -0300

    ADD - conection string to postgres

--- Sagui.Base/Comum/DALComum.cs
+++ Sagui.Base/Comum/DALComum.cs
@@ -11,5 +11,6 @@ namespace Sagui.Base.Comum
         public static string HandlerName = "Sagui.Base.DAL.DALBaseSQL";
         //public static string HandlerConnection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=sagui;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public static string HandlerConnection = @"Host=localhost;Port=5432;Pooling=true;Database=Sagui;User Id=postgres;Password=!!br4v05;";
+        //public static string HandlerConnection = @"Host=ec2-54-221-243-211.compute-1.amazonaws.com;Port=5432;Pooling=true;Database=d2u2lp40bjf8fk;User Id=ycydyagzcivkxd;Password=c71d3cfdc47cde4b75acda03dbdf1b8299871814aaa1ed6b7a16c6f3c6ae951c;";
     }
 }
