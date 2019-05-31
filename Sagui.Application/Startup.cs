11735b68c56775da0365cbc2b4f3c723c82dd1c5
Fabio Santos <fabio.silva@tecfil.com.br>
2019-05-31 19:39:23 -0300

    FIX swagger

--- Sagui.Application/Startup.cs
+++ Sagui.Application/Startup.cs
@@ -69,12 +69,11 @@ namespace Sagui.Application
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
-                      
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sagui API V1");
             });
+
 
         }
     }
