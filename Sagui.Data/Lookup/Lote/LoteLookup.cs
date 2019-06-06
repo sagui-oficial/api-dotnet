68ab948bed354b64cebfe628f11b89e411903e8e
Andre Luis Miranda <almiranda86@gmail.com>
2019-06-06 08:50:22 -0300

    Correcao merge

--- Sagui.Data/Lookup/Lote/LoteLookup.cs
+++ Sagui.Data/Lookup/Lote/LoteLookup.cs
@@ -61,43 +61,5 @@ namespace Sagui.Data.Lookup.Lote
 
             return Lote;
         }
-
-        public Model.Lote ObterLote(Model.Lote lote)
-        {
-            if (lote == null)
-                throw new ArgumentNullException(nameof(lote));
-
-            DbParams.Add(nameof(lote.PublicID), lote.PublicID.ToString());
-
-            using (DataInfrastructure dataInfrastructure = DataInfrastructure.GetInstanceDb(SQL.ObterLotebyPublicID, DbParams))
-            {
-                try
-                {
-                    var reader = dataInfrastructure.command.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        Model.Lote _Lote = new Model.Lote();
-                        _Lote.Id = Convert.ToInt32(reader["Id"]);
-                        _Lote.Status = Convert.ToInt32(reader["Status"]);
-                        _Lote.PlanoOperadora = new Model.PlanoOperadora();
-                        _Lote.PlanoOperadora.Id = Convert.ToInt32(reader["PlanoOperadoraId"]);
-                        _Lote.PlanoOperadora.NomeFantasia = Convert.ToString(reader["NomeFantasia"]);
-                        _Lote.PlanoOperadora.RazaoSocial = Convert.ToString(reader["RazaoSocial"]);
-                        _Lote.PublicID = (Guid)reader["PublicID"];
-                        _Lote.TotalGTOLote = Convert.ToInt32(reader["TotalProcedimentos"]);
-                        _Lote.ValorTotalLote = Convert.ToDouble(reader["ValorTotalProcedimentos"]);
-                        lote = _Lote;
-                    }
-                }
-                catch (Exception e)
-                {
-                    lote = null;
-                }
-
-            }
-
-            return lote;
-        }
     }
 }
