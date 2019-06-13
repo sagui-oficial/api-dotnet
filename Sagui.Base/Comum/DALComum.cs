8bd1ea418b1dd7784338da816208b0015851b674
Fabio Santos <fabio.silva@tecfil.com.br>
2019-06-13 16:44:59 -0300

    teste

--- Sagui.Base/Comum/DALComum.cs
+++ Sagui.Base/Comum/DALComum.cs
@@ -10,6 +10,7 @@ namespace Sagui.Base.Comum
     {
         public static string HandlerName = "Sagui.Base.DAL.DALBaseSQL";
         //public static string HandlerConnection = @"Host=localhost;Port=5432;Pooling=true;Database=Sagui;User Id=postgres;Password=!!br4v05;";
+        //Teste
         public static string HandlerConnection = @"Host=ec2-50-19-221-38.compute-1.amazonaws.com;Port=5432;Pooling=true;Database=dcb6ffcg9nsm0r;User Id=ltoqphzeigcytl;Password=14c5b7f6643cad4b778c36a0c5629646a390615bc441bf4894c2c2d343ff965f;";
     }
 }
