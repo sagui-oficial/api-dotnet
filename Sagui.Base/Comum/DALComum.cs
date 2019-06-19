c37f61de4e11e1f37486bcb2054f78ba0cf4c4b3
Fabio Santos <fabiosnts@gmail.com>
2019-06-19 00:01:15 +0000

    Update Sagui.Base/Comum/DALComum.cs

--- Sagui.Base/Comum/DALComum.cs
+++ Sagui.Base/Comum/DALComum.cs
@@ -11,7 +11,8 @@ namespace Sagui.Base.Comum
         public static string HandlerName = "Sagui.Base.DAL.DALBaseSQL";
         //public static string HandlerConnection = @"Host=localhost;Port=5432;Pooling=true;Database=Sagui;User Id=postgres;Password=!!br4v05;";
         //Teste
-        public static string HandlerConnection = @"Host=ec2-50-19-221-38.compute-1.amazonaws.com;Port=5432;Pooling=true;Database=dcb6ffcg9nsm0r;User Id=ltoqphzeigcytl;Password=14c5b7f6643cad4b778c36a0c5629646a390615bc441bf4894c2c2d343ff965f;";
+        public static string HandlerConnection = @"Host=
+ec2-174-129-27-3.compute-1.amazonaws.com;Port=5432;Pooling=true;Database=dadhakti70ocr;User Id=zcfzoskjnteuaj;Password=611d20c029b21551e78b2cd3629a8623e2017b8ad61157340610e3dc7af8a994;SSL Mode=Require;Trust Server Certificate=true;";
 
     }
 }
